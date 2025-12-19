3f94f87a856a26c15d08e8306890d2337214943a
Varma0313 <ravivarmasri13@gmail.com>
2025-12-19 08:57:40 +0530

    Sort changes

--- src/app/components/pipes/todo-sort.pipe.ts
+++ src/app/components/pipes/todo-sort.pipe.ts
@@ -9,11 +9,27 @@ export class TodoSortPipe implements PipeTransform {
   transform(value: any, sortBy?: 'completed' | 'created' | null): any {
     console.log('check Value', value);
     if (!sortBy) return this.sortByTimeDesc(value);
+    if (sortBy === 'completed') return this.SortByCompleted(value);
+    if (sortBy === 'created') return this.sortByCreated(value);
     if (sortBy) return [];
     return value;
   }
 
   sortByTimeDesc(arr: IToDo[]) {
+    return arr.sort((a: IToDo, b: IToDo) => {
+      return a.time - b.time;
+    });
+  }
+
+  SortByCompleted(arr: IToDo[]) {
+    let completedCollection: IToDo[] = [];
+    let nonCompletedCollection: IToDo[] = [];
+    completedCollection = arr.filter((item) => item.isCompleted);
+    nonCompletedCollection = arr.filter((item) => !item.isCompleted);
+    return [...nonCompletedCollection, ...completedCollection];
+  }
+
+  sortByCreated(arr: IToDo[]) {
     return arr.sort((a: IToDo, b: IToDo) => {
       return a.time - b.time;
     });
